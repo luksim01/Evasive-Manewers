ec30d9cf94188c5127d52bab6984c0ec409a0b5b
luksim01 <145286746+luksim01@users.noreply.github.com>
2024-11-10 15:08:56 +0000

    unity test runner framework
    - setup test runner for edit mode
    - generated script assembly
    - package references in assemblies

---
+++ Assets/Tests/EditMode/DirectionTests.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+public class DirectionTests
+{
+    [Test]
+    public void DirectionTestsSimplePasses()
+    {
+        // Use the Assert class to test conditions
+    }
+}
